97b476c84b0d85488caf5d1353821b0548464985
ihwang1 <irishwang0104@gmail.com>
2019-04-23 13:25:24 -0400

    Add code

---
+++ Final Project/Background_Sub.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2 as cv
+cap = cv.VideoCapture('Test_Small.mp4')
+kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(3,3))
+fgbg = cv.bgsegm.createBackgroundSubtractorGMG(60)
+while(1):
+    ret, frame = cap.read()
+    fgmask = fgbg.apply(frame)
+
+    fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
+    foreground_mask = cv.bitwise_and(frame,frame,mask=255 - fgmask)
+    foreground_mask= cv.inpaint(foreground_mask,fgmask,3,cv.INPAINT_TELEA)
+    #background_mask = cv.bitwise_and(frame,frame,mask=fgmask)
+    #background_mask = cv.inpaint(background_mask,fgmask,3,cv.INPAINT_TELEA)
+    #print(frame.shape, fgmask.shape, realmask.shape)
+    cv.imshow('frame',foreground_mask)
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+cap.release()
+cv.destroyAllWindows()
