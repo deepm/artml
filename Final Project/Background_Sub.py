5020524c1ccf5acd6d1bb47602d8ac3440385b01
ihwang1 <irishwang0104@gmail.com>
2019-04-24 11:24:15 -0400

    Update Background_Sub.py

--- Final Project/Background_Sub.py
+++ Final Project/Background_Sub.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2 as cv
-cap = cv.VideoCapture('Test_Small.mp4')
+cap = cv.VideoCapture('fish1.mp4')
 kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(3,3))
-fgbg = cv.bgsegm.createBackgroundSubtractorGMG(60)
+fgbg = cv.bgsegm.createBackgroundSubtractorGMG(10)
 while(1):
     ret, frame = cap.read()
     fgmask = fgbg.apply(frame)
@@ -10,8 +10,9 @@ while(1):
     fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
     foreground_mask = cv.bitwise_and(frame,frame,mask=255 - fgmask)
     foreground_mask= cv.inpaint(foreground_mask,fgmask,3,cv.INPAINT_TELEA)
+    background_mask = cv.bitwise_and(frame,frame,mask=fgmask)
     
-    cv.imshow('frame',foreground_mask)
+    cv.imshow('frame',background_mask)
     k = cv.waitKey(30) & 0xff
     if k == 27:
         break
